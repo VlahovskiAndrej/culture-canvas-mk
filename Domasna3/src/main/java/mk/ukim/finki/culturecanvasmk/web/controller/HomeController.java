d78e2b4883c6fd5c5557841d8b97b08a9f6b0905
akrango <angelakrango27@gmail.com>
2023-12-09 05:11:46 +0100

    Fixing templates

--- Domasna3/src/main/java/mk/ukim/finki/culturecanvasmk/web/controller/HomeController.java
+++ Domasna3/src/main/java/mk/ukim/finki/culturecanvasmk/web/controller/HomeController.java
@@ -3,6 +3,7 @@ package mk.ukim.finki.culturecanvasmk.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,6 +24,11 @@ public class HomeController {
 
     @GetMapping("/contact")
     public String getContactPage(Model model) {
+        model.addAttribute("bodyContent", "contact");
+        return "master-template";
+    }
+    @PostMapping("/contact")
+    public String sendEmail(Model model) {
         model.addAttribute("bodyContent", "contact");
         return "master-template";
     }
