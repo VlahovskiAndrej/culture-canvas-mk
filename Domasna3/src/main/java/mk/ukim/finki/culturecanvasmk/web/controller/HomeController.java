faeccffe6a646a548a11f64961b6e41893df4812
akrango <angelakrango27@gmail.com>
2023-12-09 02:49:49 +0100

    Added header, footer and initial pages

---
+++ Domasna3/src/main/java/mk/ukim/finki/culturecanvasmk/web/controller/HomeController.java
@@ -0,0 +1,29 @@
+package mk.ukim.finki.culturecanvasmk.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/home", "/"})
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage(Model model) {
+        model.addAttribute("bodyContent", "home");
+        return "master-template";
+    }
+
+    @GetMapping("/about")
+    public String getAboutPage(Model model) {
+        model.addAttribute("bodyContent", "about");
+        return "master-template";
+    }
+
+    @GetMapping("/contact")
+    public String getContactPage(Model model) {
+        model.addAttribute("bodyContent", "contact");
+        return "master-template";
+    }
+}
