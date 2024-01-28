bae8cf58185eb31727c18b60614629af90454b25
Andrej Vlahovski <vlahovskiandrej@yahoo.com>
2024-01-28 15:45:02 +0100

    deployment structure set

--- Domasna3,4/CultureCanvasMK/src/main/java/mk/ukim/finki/culturecanvasmk/model/exceptions/NoReviewFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.culturecanvasmk.model.exceptions;
-
-public class NoReviewFoundException extends RuntimeException{
-    public NoReviewFoundException(Long id) {
-        super(String.format("Review with id %d couldn't be found",id));
-    }
-}
